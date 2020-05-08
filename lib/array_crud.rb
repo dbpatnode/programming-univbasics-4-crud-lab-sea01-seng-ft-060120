0292e2f27a73ab00c51ab6bb7fde948f5774dc34
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-08 16:22:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   new_array =["mom","dad","sister","brother"]
-  new_array.unshift("grandma")
+  new_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
