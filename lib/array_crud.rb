0e820651e93b16686096008d6a8d6520c927fcac
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-08 16:50:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,5 +41,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+new_array = ["wow","mom","dad","sister","brother"]
+swapped_out_element[0] = "totaly"
 end
