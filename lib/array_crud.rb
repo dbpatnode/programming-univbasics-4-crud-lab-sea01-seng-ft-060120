fd09a8c12e3d8cca6c6178d90fae40ab71de82bf
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-08 16:22:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   new_array =["mom","dad","sister","brother"]
-  new_array.push("grandma")
+  new_array.push("arrays")
 end
 
 def add_element_to_start_of_array(array, element)
